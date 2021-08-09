ec525c72c306d53326eff08c19b6e7c4ac005214
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-09 16:14:06 -0300

    feat: Printing the table of 5.

---
+++ 03-node-bases/app.js
@@ -0,0 +1,12 @@
+console.clear();
+
+console.log('================');
+console.log('Tabla del 5');
+console.log('================');
+
+const base = 5;
+const limit = 10;
+
+for(let i = 1; i <= limit; i++) {
+	console.log(`${ base } X ${ i } = ${ base * i }`);
+}
