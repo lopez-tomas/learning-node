2a6c0b34eeee1e495676385f1951f49a5ae4d6da
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-09 17:22:39 -0300

    fix: Now, the code is cleaner. I'm importing the function in multiply.js in helpers directory.

--- 03-node-bases/app.js
+++ 03-node-bases/app.js
@@ -1,30 +1,8 @@
-const fs = require('fs');
-// import { writeFile } from 'fs'; => new standard to import in ES6
+const { createFile } = require('./helpers/multiply');
 
 console.clear();
 
-const base = 3;
+const base = 5;
 const limit = 10;
 
-console.log('================');
-console.log(`Table of ${base}`);
-console.log('================');
-
-let output = '';
-
-for(let i = 1; i <= limit; i++) {
-	output += `${ base } X ${ i } = ${ base * i }\n`;
-}
-
-console.log(output);
-
-//fs.writeFile(`table-${base}.txt`, output, (err) => {
-//	if (err) throw err;
-//
-//	console.log(`table-${base}.txt file created.`);
-//})
-
-fs.writeFileSync( `table-${base}.txt`, output );
-// To manage erros with writeFileSync we have to use try and catch.
-
-console.log(`table-${base}.txt file created.`);
+createFile(base, limit);
