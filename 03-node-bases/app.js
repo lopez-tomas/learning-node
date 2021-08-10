0d11a9473d56d4faf84bc7035d7402750ef95164
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-10 16:11:09 -0300

    feat: Basic managing with console parameters.

--- 03-node-bases/app.js
+++ 03-node-bases/app.js
@@ -2,7 +2,10 @@ const { printTable, createFile } = require('./helpers/multiply');
 
 console.clear();
 
-const base = 5;
+const [ , , arg3 = 'base=5' ] = process.argv;
+const [ , base = 5 ] = arg3.split('=');
+
+//const base = 5;
 const limit = 10;
 
 createFile( base, limit )
