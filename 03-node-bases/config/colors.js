a2f5c75b8f36035492a4287e911023f93cdd8c3c
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-18 20:12:53 -0300

    feat: New config file created and exported for colors package. It has some variables to use.

---
+++ 03-node-bases/config/colors.js
@@ -0,0 +1,20 @@
+const colors = require('colors');
+
+colors.setTheme({
+	headerTitleBg: 'bgYellow',
+	headerStyle: 'bold',
+	headerLines: 'rainbow',
+	number: 'yellow',
+	result: 'magenta',
+	resultBg: 'bgWhite',
+	resultStyle: 'bold',
+	crux: 'blue',
+	cruxStyle: 'bold',
+	file: 'rainbow',
+	success: 'blue',
+	error: 'red'
+});
+
+module.exports = colors;
+
+
