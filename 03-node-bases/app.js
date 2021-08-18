070230e94cebeac888e33ee344a94895e4ed87e8
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-18 20:14:34 -0300

    feat: colors' file imported to give some style on the console.

--- 03-node-bases/app.js
+++ 03-node-bases/app.js
@@ -1,9 +1,10 @@
 const { printTable, createFile } = require('./helpers/multiply');
 const argv = require('./config/yargs');
+const colors = require('./config/colors');
 
 console.clear();
 
-//console.log( argv ); => To know how are represented the values and my configs.
+//console.log( argv ); => To know how are represented the values and my Yargs' configs.
 let	base = argv.base;
 let limit = argv.limit;
 let show = argv.show;
@@ -13,8 +14,8 @@ let show = argv.show;
 // I am not going to use it anymore, but I leave this part of the code for 'documentation'.
 
 createFile( base, limit )
-	.then( fileName => console.log(fileName, 'succesfully created.') )
-	.catch( err => console.log(err) );
+	.then( fileName => console.log(fileName.file.bold, 'succesfully created.'.success) )
+	.catch( err => console.log(err.error) );
 
 if ( show ) {
 	printTable( base,  limit );
