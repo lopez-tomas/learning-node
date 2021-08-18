4feda29c957a9e5f43e8228b91a8d80964234a28
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-18 18:37:01 -0300

    feat: Yargs config imported into main file.

--- 03-node-bases/app.js
+++ 03-node-bases/app.js
@@ -1,40 +1,9 @@
 const { printTable, createFile } = require('./helpers/multiply');
-const argv = require('yargs')
-								.options({
-									'b': {
-										alias: 'base',
-										type: 'number',
-										demandOption: true,
-										describe: 'To establish the base.'
-									},
-									'l': {
-										alias: 'limit',
-										type: 'number',
-										demandOption: true,
-										describe: 'To establish the limit.'
-									},
-									's': {
-										alias: 'show',
-										type: 'boolean',
-										default: false,
-										describe: 'Prints on console the corresponding multiplication table.'
-									}
-								})								
-								.check( (argv, options) => {
-									if( isNaN( argv.b )) {
-										throw 'The base must be a number.'
-									}
-									if( isNaN( argv.l )) {
-										throw 'The limit must be a number.'
-									}
-									return true;
-								})
-								.argv;
-	
+const argv = require('./config/yargs');
 
 console.clear();
 
-console.log( argv );
+//console.log( argv ); => To know how are represented the values and my configs.
 let	base = argv.base;
 let limit = argv.limit;
 let show = argv.show;
