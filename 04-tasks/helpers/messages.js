4998af9b5488b8571d3eeaf67f1845ea55233822
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-18 23:45:10 -0300

    feat: Menu options created. They're not functinal yet.

--- 04-tasks/helpers/messages.js
+++ 04-tasks/helpers/messages.js
@@ -6,7 +6,13 @@ const showMenu = () => {
   console.log(' Select one option'.header);
   console.log('===================\n'.header);
 
-  console.log(``);
+  console.log(`1. Create task`);
+  console.log(`2. List tasks`);
+  console.log(`3. List completed tasks`);
+  console.log(`4. List pending tasks`);
+  console.log(`5. Complete task(s)`);
+  console.log(`6. Delete task`);
+  console.log(`0. Exit \n`);
 };
 
 module.exports = {
