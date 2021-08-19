b8b0aae880f97caa4237b4e0513b3d9e322f9cbe
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-18 23:41:36 -0300

    feat: Folder to group all the specific functions of my console application.

---
+++ 04-tasks/helpers/messages.js
@@ -0,0 +1,14 @@
+const colors = require('../config/colors');
+
+const showMenu = () => {
+  console.clear;
+  console.log('==================='.header);
+  console.log(' Select one option'.header);
+  console.log('===================\n'.header);
+
+  console.log(``);
+};
+
+module.exports = {
+  showMenu
+}
