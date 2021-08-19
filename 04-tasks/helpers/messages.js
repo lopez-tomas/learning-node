1a80d3c4c1be4cf3bb0025ee4cc675d9d23bf0a9
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-19 00:34:15 -0300

    feat: Now showMenu & pause functions have a Promise for the option.

--- 04-tasks/helpers/messages.js
+++ 04-tasks/helpers/messages.js
@@ -1,30 +1,47 @@
 const colors = require('../config/colors');
 
 const showMenu = () => {
-  console.clear;
-  console.log('==================='.header);
-  console.log(' Select one option'.header);
-  console.log('===================\n'.header);
+  return new Promise( resolve => {
+    console.clear;
+    console.log('==================='.header);
+    console.log(' Select one option'.header);
+    console.log('===================\n'.header);
 
-  console.log(`${ '1.'.option } Create task`);
-  console.log(`${ '2.'.option } List tasks`);
-  console.log(`${ '3.'.option } List completed tasks`);
-  console.log(`${ '4.'.option } List pending tasks`);
-  console.log(`${ '5.'.option } Complete task(s)`);
-  console.log(`${ '6.'.option } Delete task`);
-  console.log(`${ '0.'.option } Exit \n`);
+    console.log(`${ '1.'.option } Create task`);
+    console.log(`${ '2.'.option } List tasks`);
+    console.log(`${ '3.'.option } List completed tasks`);
+    console.log(`${ '4.'.option } List pending tasks`);
+    console.log(`${ '5.'.option } Complete task(s)`);
+    console.log(`${ '6.'.option } Delete task`);
+    console.log(`${ '0.'.option } Exit \n`);
 
-  const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+    const readline = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  readline.question('Select one option: ', (opt) => {
-    console.log({ opt });
-    readline.close();
+    readline.question('Select one option: ', (opt) => {
+      readline.close();
+      resolve(opt);
+    })
+  })
+};
+
+const pause = () => {
+  return new Promise( resolve => {
+    const readline = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    readline.question(`\nPress ${ 'ENTER'.enter } to continue\n`, (opt) => {
+      readline.close();
+      resolve();
+    })
   })
 };
 
 module.exports = {
-  showMenu
+  showMenu,
+  pause
 }
