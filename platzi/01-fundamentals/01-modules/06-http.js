d177b5168040de19bafcdd6cbce7d175344623bf
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-08 15:51:39 -0300

    [FEAT]: learning about HTTP in Node.js

---
+++ platzi/01-fundamentals/01-modules/06-http.js
@@ -0,0 +1,27 @@
+const http = require('http');
+
+const router = (req, res) => {
+  console.log('New petition');
+  console.log(req.url);
+
+  switch(req.url) {
+    case '/hola':
+      res.write('Hi, how are you?');
+      res.end();
+      break;
+    default:
+      res.write('Error 404');
+      res.end();
+  }
+
+  // res.writeHead(201, {
+  //   'Content-Type': 'text/plain'
+  // })
+  // res.write("I'm using HTTP from Node.js");
+
+  // res.end();
+}
+
+http.createServer(router).listen(3000); // 3000, 3001, 3002,... 8080, 8081, ...
+
+console.log('Listening on port 3000');
