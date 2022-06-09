625a5d750be1139168c34577fa4f344fb21074cc
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-09 12:39:19 -0300

    [FEAT]: create, update & delete methods added in CategoriesService.

--- platzi/02-api-rest/src/api/services/category.service.ts
+++ platzi/02-api-rest/src/api/services/category.service.ts
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker';
 import { Category } from '../interfaces/categories/category.model';
+import { CreateCategoryDto, UpdateCategoryDto } from '../interfaces/categories/category.dto';
 
 class CategoriesService {
   categories: Category[];
@@ -22,12 +23,43 @@ class CategoriesService {
     }
   }
 
+  create(data: CreateCategoryDto) {
+    const newCategory: Category = {
+      id: faker.datatype.uuid(),
+      ...data
+    }
+
+    this.categories.push(newCategory);
+    return newCategory;
+  }
+
   get() {
     return this.categories;
   }
 
   getCategory(id: Category['id']) {
     return this.categories.find(user => user.id === id);
+  }
+
+  update(id: Category['id'], changes: UpdateCategoryDto) {
+    const index = this.categories.findIndex(category => category.id === id);
+
+    if (index === -1) throw new Error('category not found');
+
+    const prevData = this.categories[index];
+    this.categories[index] = {...prevData, ...changes};
+
+    return this.categories[index];
+  }
+
+  delete(id: Category['id']) {
+    const index = this.categories.findIndex(category => category.id === id);
+
+    if (index === -1) throw new Error('category not found');
+
+    this.categories.splice(index, 1);
+
+    return { id };
   }
 }
 
