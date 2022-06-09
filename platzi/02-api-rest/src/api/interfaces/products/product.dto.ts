42c53163ed6d85394053dfa22e196f46081e0207
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-09 17:39:38 -0300

    [FEAT]: createProductSchema, getProductSchema & updateProductSchema schemas created with Joi & exported.

--- platzi/02-api-rest/src/api/interfaces/products/product.dto.ts
+++ platzi/02-api-rest/src/api/interfaces/products/product.dto.ts
@@ -1,5 +1,42 @@
-import { Product  } from "./product.model";
+import { Product } from "./product.model";
+import Joi from 'joi';
 
 export type CreateProductDto = Omit<Product, 'id'>;
 
 export type UpdateProductDto = Partial<CreateProductDto>;
+
+const id = Joi.string().uuid();
+const name = Joi.string().min(3);
+const price = Joi.number().min(1.0);
+const description = Joi.string().min(10);
+const image = Joi.string().dataUri();
+const idCategory = Joi.string().uuid();
+const isBlocked = Joi.boolean();
+
+const createProductSchema = Joi.object({
+  name: name.required(),
+  price: price.required(),
+  description: description.required(),
+  image: image,
+  idCategory: idCategory.required(),
+  isBlocked: isBlocked.required(),
+});
+
+const getProductSchema = Joi.object({
+  id: id.required(),
+});
+
+const updateProductSchema = Joi.object({
+  name: name,
+  price: price,
+  description: description,
+  image: image,
+  idCategory: idCategory,
+  isBlocked: isBlocked,
+});
+
+export {
+  createProductSchema,
+  getProductSchema,
+  updateProductSchema
+}
