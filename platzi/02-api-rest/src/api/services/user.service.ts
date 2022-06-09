e4349b32ebcea1ef1d21651f74afd54682a529bb
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-09 12:32:27 -0300

    [FEAT]: create, update & delete methods added in UsersService.

--- platzi/02-api-rest/src/api/services/user.service.ts
+++ platzi/02-api-rest/src/api/services/user.service.ts
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker';
 import { User } from '../interfaces/users/user.model';
+import { CreateUserDto, UpdateUserDto } from '../interfaces/users/user.dto';
 
 class UsersService {
   users: User[];
@@ -23,12 +24,43 @@ class UsersService {
     }
   }
 
+  create(data: CreateUserDto) {
+    const newUser: User = {
+      id: faker.datatype.uuid(),
+      ...data
+    }
+
+    this.users.push(newUser);
+    return newUser;
+  }
+
   get() {
     return this.users;
   }
 
   getUser(id: User['id']) {
     return this.users.find(user => user.id === id);
+  }
+
+  update(id: User['id'], changes: UpdateUserDto) {
+    const index = this.users.findIndex(user => user.id === id);
+
+    if (index === -1) throw new Error('user not found');
+
+    const prevData = this.users[index];
+    this.users[index] = {...prevData, ...changes};
+
+    return this.users[index];
+  }
+
+  delete(id: User['id']) {
+    const index = this.users.findIndex(user => user.id === id);
+
+    if (index === -1) throw new Error('user not found');
+
+    this.users.splice(index, 1);
+
+    return { id };
   }
 }
 
