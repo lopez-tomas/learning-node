d2241ea3e4d39bde03e87b4f2b2a63718864292d
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-09 17:42:48 -0300

    [FEAT]: index created to export & import interfaces, types & schemas from one place.

---
+++ platzi/02-api-rest/src/api/interfaces/products/index.ts
@@ -0,0 +1,17 @@
+import { Product } from './product.model';
+import {
+  CreateProductDto,
+  UpdateProductDto,
+  createProductSchema,
+  getProductSchema,
+  updateProductSchema
+} from './product.dto';
+
+export {
+  Product,
+  CreateProductDto,
+  UpdateProductDto,
+  createProductSchema,
+  getProductSchema,
+  updateProductSchema,
+}
