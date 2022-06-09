7eab50eb8f6345679e6a92e4f60668503671f744
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-09 13:24:20 -0300

    [FEAT]: try & catch added.

--- platzi/02-api-rest/src/api/routes/products.router.ts
+++ platzi/02-api-rest/src/api/routes/products.router.ts
@@ -5,8 +5,8 @@ import ProductsService from '../services/product.service';
 const router: Router = express.Router();
 const service = new ProductsService();
 
-router.get('/', (req: Request, res: Response) => {
-  const products = service.get();
+router.get('/', async (req: Request, res: Response) => {
+  const products = await service.get();
   res.status(HttpStatusCode.OK).json(products);
 });
 
@@ -14,42 +14,61 @@ router.get('/filter', (req: Request, res: Response) => {
   res.send('I am a filter');
 });
 
-router.get('/:id', (req: Request, res: Response) => {
+router.get('/:id', async (req: Request, res: Response) => {
   const { id } = req.params;
-  const product = service.getProduct(id);
 
-  if (product) {
+  try {
+    const product = await service.getProduct(id);
     res.status(HttpStatusCode.OK).json(product);
-  } else {
-    res.status(HttpStatusCode.BAD_REQUEST).json({
-      message: 'product not found'
-    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(HttpStatusCode.NOT_FOUND).json({
+        message: error.message
+      });
+    }
   }
 });
 
-router.post('/', (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
   const body = req.body;
-  const newProduct = service.create(body);
+  const newProduct = await service.create(body);
 
   res.status(HttpStatusCode.CREATED).json(newProduct);
 });
 
-router.patch('/:id', (req: Request, res: Response) => {
+router.patch('/:id', async (req: Request, res: Response) => {
   const { id } = req.params;
   const body = req.body;
-  const product = service.update(id, body);
 
-  res.status(HttpStatusCode.ACCEPTED).json(product);
+  try {
+    const product = await service.update(id, body);
+    res.status(HttpStatusCode.ACCEPTED).json(product);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(HttpStatusCode.NOT_FOUND).json({
+        message: error.message
+      });
+    }
+  }
 });
 
-router.delete('/:id', (req: Request, res: Response) => {
+router.delete('/:id', async (req: Request, res: Response) => {
   const { id } = req.params;
-  const response = service.delete(id);
 
-  res.status(HttpStatusCode.OK).json({
-    message: 'product deleted',
-    response
-  });
+  try {
+    const response = await service.delete(id);
+    res.status(HttpStatusCode.OK).json({
+      message: 'product deleted',
+      response
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(HttpStatusCode.NOT_FOUND).json({
+        message: error.message
+      });
+    }
+  }
+
 });
 
 export {
