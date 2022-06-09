6f709696ef276b3bfbb7c2eb38b626e43233039d
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-09 00:21:48 -0300

    [FEAT]: new endpoint /api/v1 created using app.use() & then using router.use() for each router.

--- platzi/02-api-rest/src/api/routes/index.ts
+++ platzi/02-api-rest/src/api/routes/index.ts
@@ -1,13 +1,16 @@
-import { Express } from 'express';
+import { Router, Express } from 'express';
 
 import { router as usersRouter } from './users.router';
 import { router as productsRouter } from './products.router';
 import { router as categoriesRouter } from './categories.router';
 
 const routerApi = (app: Express) => {
-  app.use('/api/users', usersRouter);
-  app.use('/api/products', productsRouter);
-  app.use('/api/categories', categoriesRouter);
+  const router: Router = Router();
+  app.use('/api/v1', router);
+
+  router.use('/users', usersRouter);
+  router.use('/products', productsRouter);
+  router.use('/categories', categoriesRouter);
 }
 
 export default routerApi;
