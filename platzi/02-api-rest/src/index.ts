742df317062630b3a835b046d67ef155c3d0412b
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-10 19:22:11 -0300

    [FEAT]: using cors with options.

--- platzi/02-api-rest/src/index.ts
+++ platzi/02-api-rest/src/index.ts
@@ -1,12 +1,26 @@
 import express, { Request, Response } from 'express';
-import routerApi from './api/routes';
+import cors from 'cors';
 
+import routerApi from './api/routes';
 import { logErrors, errorHandler, boomErrorHandler } from './api/middlewares/error.handler';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whiteList = ['http://localhost:8080', 'https://tomaslopez.dev'];
+const options = {
+  origin: (origin: any, callback: any) => {
+    if (whiteList.includes(origin)) {
+      callback(null, true);
+    } else{
+      callback(new Error('Origin not allowed: ' + origin))
+    }
+  }
+}
+
+app.use(cors(options));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
