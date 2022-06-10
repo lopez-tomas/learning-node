1938894238916cbc164d434cee19a6c528b06749
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-10 16:28:09 -0300

    [FEAT]: create, get & update schemas created for Users & Categories.

--- platzi/02-api-rest/src/api/interfaces/categories/category.dto.ts
+++ platzi/02-api-rest/src/api/interfaces/categories/category.dto.ts
@@ -1,5 +1,27 @@
 import { Category } from './category.model';
+import Joi from 'joi';
 
 export type CreateCategoryDto = Omit<Category, 'id'>;
 
 export type UpdateCategoryDto = Partial<CreateCategoryDto>;
+
+const id = Joi.string().uuid();
+const category = Joi.string();
+
+const createCategorySchema = Joi.object({
+  category: category.required(),
+});
+
+const getCategorySchema = Joi.object({
+  id: id.required(),
+});
+
+const updateCategorySchema = Joi.object({
+  category: category.required(),
+});
+
+export {
+  createCategorySchema,
+  getCategorySchema,
+  updateCategorySchema,
+}
--- platzi/02-api-rest/src/api/interfaces/users/user.dto.ts
+++ platzi/02-api-rest/src/api/interfaces/users/user.dto.ts
@@ -1,5 +1,30 @@
 import { User } from "./user.model";
+import Joi from 'joi';
 
 export type CreateUserDto = Omit<User, 'id'>;
 
 export type UpdateUserDto = Partial<CreateUserDto>;
+
+const id = Joi.string().uuid();
+const name = Joi.string().min(3);
+const email = Joi.string().email();
+
+const createUserSchema = Joi.object({
+  name: name.required(),
+  email: email.required(),
+});
+
+const getUserSchema = Joi.object({
+  id: id.required(),
+});
+
+const updateUserSchema = Joi.object({
+  name: name,
+  email: email,
+});
+
+export {
+  createUserSchema,
+  getUserSchema,
+  updateUserSchema,
+}
