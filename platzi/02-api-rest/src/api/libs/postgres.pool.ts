d37cc2d339fdc15acd28c9d4e9f7aa36e6d0ea13
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-14 16:37:30 -0300

    [FEAT]: connection to Postgres with Client & Pool from -pg-. We are going to use Pool.

---
+++ platzi/02-api-rest/src/api/libs/postgres.pool.ts
@@ -0,0 +1,12 @@
+import { config } from '../../config/config';
+import { Pool } from 'pg';
+
+const USER = encodeURIComponent(config.dbUser!);
+const PASSWORD = encodeURIComponent(config.dbPassword!);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+const pool = new Pool({ connectionString: URI });
+
+export {
+  pool,
+}
---
+++ platzi/02-api-rest/src/api/libs/postgres.ts
@@ -0,0 +1,16 @@
+import { config } from '../../config/config';
+import { Client } from 'pg';
+
+const USER = encodeURIComponent(config.dbUser!);
+const PASSWORD = encodeURIComponent(config.dbPassword!);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+const getConnection = async () => {
+  const client = new Client({ connectionString: URI });
+  await client.connect();
+  return client;
+}
+
+export {
+  getConnection,
+}
