58f0fbd75c47e45c0ac272b35a96b013af3a84de
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-16 14:47:47 -0300

    [FEAT]: using MySQL instead of Postgres.

--- platzi/02-api-rest/src/config/config.ts
+++ platzi/02-api-rest/src/config/config.ts
@@ -6,9 +6,9 @@ const config = {
   port: process.env.PORT || 3000,
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
-  dbUser: process.env.DB_USER,
+  dbUser: process.env.MYSQL_USER,
   dbPassword: process.env.DB_PASSWORD,
-  dbPort: process.env.DB_PORT,
+  dbPort: process.env.MYSQL_MY_PORT,
 }
 
 export {
